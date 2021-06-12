1173172e1dc7acd5c7bba617c093e08ff3842233
ivan <rybnikovv12345@gmail.com>
2021-06-12 10:12:28 +0300

    [+] create projects

---
+++ FoodOrder/src/popIT.FoodOrder.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace popIT.FoodOrder.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ FoodOrder/src/popIT.FoodOrder.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace popIT.FoodOrder.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
