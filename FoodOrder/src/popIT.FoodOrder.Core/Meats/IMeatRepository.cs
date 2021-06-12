5b6dd635c1bc37587ec333bc523abca60806cba8
Alex <ffgh04@gmail.com>
2021-06-12 11:58:09 +0400

    Create Meat and Soup interface repositories

--- FoodOrder/src/popIT.FoodOrder.Core/Meats/IMeatRepository.cs
+++ FoodOrder/src/popIT.FoodOrder.Core/Meats/IMeatRepository.cs
@@ -3,11 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Query;
 
 namespace popIT.FoodOrder.Core.Meats
 {
     public interface IMeatRepository
     {
-        
+        Task<IEnumerable<Meat>> GetAllMeats(
+            Expression<Func<Meat, bool>> predicate = null,
+            Func<IQueryable<Meat>, IOrderedQueryable<Meat>> orderBy = null,
+            Func<IQueryable<Meat>, IIncludableQueryable<Meat, object>> include = null,
+            bool disableTracking = true
+        );
+
+        Task<Meat> GetMeatById(
+            Guid id,
+            Func<IQueryable<Meat>, IIncludableQueryable<Meat, object>> include = null,
+            bool disableTracking = true
+        );
+
+        Task AddMeat(Meat meat);
+
+        Task UpdateMeat(Meat meat);
+
+        Task DeleteMeat(Meat meat);
     }
 }
--- FoodOrder/src/popIT.FoodOrder.Core/Soups/ISoupRepository.cs
+++ FoodOrder/src/popIT.FoodOrder.Core/Soups/ISoupRepository.cs
@@ -1,7 +1,31 @@
-﻿namespace popIT.FoodOrder.Core.Soups
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Query;
+
+namespace popIT.FoodOrder.Core.Soups
 {
     public interface ISoupRepository
     {
-        
+        Task<IEnumerable<Soup>> GetAllSoups(
+            Expression<Func<Soup, bool>> predicate = null,
+            Func<IQueryable<Soup>, IOrderedQueryable<Soup>> orderBy = null,
+            Func<IQueryable<Soup>, IIncludableQueryable<Soup, object>> include = null,
+            bool disableTracking = true
+        );
+
+        Task<Soup> GetSoupById(
+            Guid id,
+            Func<IQueryable<Soup>, IIncludableQueryable<Soup, object>> include = null,
+            bool disableTracking = true
+        );
+
+        Task AddSoup(Soup meat);
+
+        Task UpdateSoup(Soup meat);
+
+        Task DeleteSoup(Soup meat);
     }
 }
