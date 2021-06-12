476687c33af7fc2cf781549bf37f713be9b0f7b0
Alex <ffgh04@gmail.com>
2021-06-12 11:28:32 +0400

    Added interface UoW

---
+++ FoodOrder/src/popIT.FoodOrder.Core/General/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace popIT.FoodOrder.Core.General
+{
+    public interface IUnitOfWork
+    {
+        Task SaveChangesAsync();
+
+        TRepository GetRepository<TRepository>();
+    }
+}
